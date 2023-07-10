fc5f8d92c6185a8895a7d9e42ae7aa478de7c177
Alyssa Wiser <alyssa.wiser@mykenzie.snhu.edu>
2023-07-10 19:52:04 -0400

    still need to edit

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
+
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">2 Years with JavaScript & React</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle"> 10+ Projects</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bxs-graduation about__icon"></i>
+
+        <h3 className="about__title">Certificate</h3>
+        <span className="about__subtitle">Full Stack Web Development</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
